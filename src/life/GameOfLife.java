4dac964195d6ae186b0227310472f2dc55126937
bergLowe <sumer2000singh@hotmail.com>
2021-03-02 21:13:33 +0530

    Commented Code | Tooltip Changes

--- src/life/GameOfLife.java
+++ src/life/GameOfLife.java
@@ -1,6 +1,5 @@
 package life;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
@@ -19,17 +18,24 @@ import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.JToggleButton;
 import javax.swing.Timer;
+import javax.swing.ToolTipManager;
 
+// Callback interface is to get the implementation
+// of updating the Generation and Alive Labels in GUI
+// after finding out next generation.
 interface Callback {
     void action(int alive, int generation);
 }
 
 class MatrixPanel extends JPanel {
 
+    // BufferedImage manipulates the image data
     public BufferedImage image;
     public GameOfLifeSetup gol;
     private Callback onUpdate;
     public int generation;
+    // Timer is a utility class, in which we can specify the future
+    // task to be executed
     public Timer timer;
     public int cellSize;
 
@@ -48,20 +54,23 @@ class MatrixPanel extends JPanel {
     }
 
     public void createImage(BufferedImage image) {
+        // Graphics allow us to draw onto the components.
         Graphics g = image.getGraphics();
         char[][] arr = this.gol.getGrid();
+
+        // Variables to traverse in the "arr" grid.
         int r = 0;
         int c = 0;
 
         onUpdate.action(this.gol.getAlive(), generation);
         this.generation += 1;
 
-        int sqW = cellSize;
+        int sqW = cellSize,
+            sqH = cellSize;
         for (int i = 0; i < cellSize * gol.getLength(); i += sqW) {
             if (r == gol.getLength()) {
                 r = 0;
             }
-            int sqH = cellSize;
             for (int j = 0; j < cellSize * gol.getLength(); j += sqH) {
                 if (c == gol.getLength()) {
                     c = 0;
@@ -78,17 +87,21 @@ class MatrixPanel extends JPanel {
             }
             r += 1;
         }
+        // Sending the old grid to set the next generation,
+        // in the grid.
         this.gol.generateGenerations(arr);
 
         repaint();
     }
 
 
+    // This method is overriden to add custom graphics and to render it.
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        // Image observer not required so it can be set to null.
-        g.drawImage(image, 0,0, null);
+        // x and y coordinates are set to 0.
+        // Image observer not required so it can be set to null in drawImage.
+        g.drawImage(image, 0, 0, null);
     }
 }
 
@@ -104,6 +117,10 @@ public class GameOfLife extends JFrame {
     public GameOfLife() {
         setTitle("Conway's Game Of Life");
 
+        // Setting up ToolTip delay time
+        ToolTipManager.sharedInstance().setDismissDelay(1500); 
+        ToolTipManager.sharedInstance().setInitialDelay(100);
+
         // Panels
         leftPanel = new JPanel();
         rightPanel = new JPanel();
@@ -112,11 +129,14 @@ public class GameOfLife extends JFrame {
         Icon restartIcon = new ImageIcon(".\\resources\\icons8-restart-30.png");
         restartButton = new JButton(restartIcon);
         restartButton.setName("ResetButton");
+        restartButton.setToolTipText("Restart");
 
         Icon pauseIcon = new ImageIcon(".\\resources\\icons8-resume-button-30.png");
         pauseButton = new JToggleButton(pauseIcon);
         pauseButton.setName("PlayToggleButton");
+        pauseButton.setToolTipText("Pause");
 
+        // Slider
         slider = new JSlider(500, 2000, 500);
         slider.setMaximumSize(new Dimension(400, 50));
         slider.setPaintTicks(true);
@@ -124,9 +144,15 @@ public class GameOfLife extends JFrame {
         slider.setPaintLabels(true);
         slider.setSnapToTicks(true);
         slider.setMajorTickSpacing(500);
+        slider.setToolTipText("Change Speed of Animation");
 
+        // Setting up Left Side of Frame
+        // leftPanel set with Box Layout
         leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.PAGE_AXIS));
         leftPanel.setBackground(new Color(197, 203, 227));
+
+        // createRigidArea creates a small vertical space between each component,
+        // to get a clear spacing.
         leftPanel.add(Box.createRigidArea(new Dimension(10, 10)));
         leftPanel.add(pauseButton);
         leftPanel.add(Box.createRigidArea(new Dimension(10, 10)));
@@ -136,8 +162,9 @@ public class GameOfLife extends JFrame {
         
         updateLabel(0);
 
+        // Creating matrixPanel object, to add it in rightPanel
         MatrixPanel matrixPanel = new MatrixPanel();
-        matrixPanel.setPreferredSize(new Dimension(matrixPanel.cellSize * matrixPanel.gol.getLength(), matrixPanel.cellSize * matrixPanel.gol.getLength()));
+        matrixPanel.setPreferredSize(new Dimension(matrixPanel.gol.getCellSize() * matrixPanel.gol.getLength(), matrixPanel.gol.getCellSize() * matrixPanel.gol.getLength()));
 
         // Setting up anonymous method using lambda because
         // there is only one method to handle in Interface.
@@ -149,45 +176,62 @@ public class GameOfLife extends JFrame {
         pauseButton.addActionListener(e -> {
             if (pauseButton.isSelected()) {
                 matrixPanel.timer.stop();
+                pauseButton.setToolTipText("Resume");
             } else {
                 matrixPanel.timer.start();
+                pauseButton.setToolTipText("Pause");
             }
         });
 
         restartButton.addActionListener(e -> {
+            // If pause button is paused then, reset it.
+            pauseButton.setSelected(false);
+            pauseButton.setToolTipText("Pause");
             matrixPanel.timer.stop();
-            matrixPanel.image = new BufferedImage(matrixPanel.cellSize * matrixPanel.gol.getLength(), matrixPanel.cellSize * matrixPanel.gol.getLength(), BufferedImage.TYPE_INT_RGB);
+            matrixPanel.image = new BufferedImage(matrixPanel.gol.getCellSize() * matrixPanel.gol.getLength(), matrixPanel.gol.getCellSize() * matrixPanel.gol.getLength(), BufferedImage.TYPE_INT_RGB);
+            // Resetting the generation to 0
             matrixPanel.generation = 0;
+            // And again define the GameOfLifeSetup 
             matrixPanel.gol = new GameOfLifeSetup();
             matrixPanel.timer.start();
         });
 
+        // To get the slider values, if changed applying it as Timer value.
         slider.addChangeListener(e -> {
             JSlider temp = (JSlider) e.getSource();
             matrixPanel.timer.setDelay(temp.getValue());
         });
 
+        // Adding the matrixPanel to rightPanel. 
+        // As matrixPanel is extended to JPanel, we can directly add it as component.
         rightPanel.add(matrixPanel);
 
+        // Dividing the frame into 1 row and 2 columns
         setLayout(new GridLayout(1, 2));
 
-        add(leftPanel, BorderLayout.PAGE_START);
-        add(rightPanel, BorderLayout.CENTER);
+        // Adding the panels into frames
+        add(leftPanel);
+        add(rightPanel);
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
+        // It centers the frame location with respect to window
+        // setLocationRelativeTo(null);
+
+        // Pack method here sizes the frame and all its component
+        // to at or above their preferred sizes.
         pack();
     }
 
+    // Initialises the Label and add it to Panel.
     public void updateLabel(int number) {
-        /*Label - 1*/
+        /* Generation Label */
         generationLabel = new JLabel();
         generationLabel.setName("GenerationLabel");
         generationLabel.setText("Generation #" + number);
         generationLabel.setFont(new Font("Comic Sans MS", Font.PLAIN, 14));
         generationLabel.setBounds(10, 5, 300, 20);
 
-        /*Label - 2*/
+        /* Alive Label */
         aliveLabel = new JLabel();
         aliveLabel.setName("AliveLabel");
         aliveLabel.setText("Alive: " + number);
--- src/life/Main.java
+++ src/life/Main.java
@@ -3,6 +3,7 @@ package life;
 import java.io.IOException;
 import java.util.Random;
 
+// Generation interface contains the main operations related to Game of Life.
 interface Generation {
 
     static char[][] generator(char[][] grid, int length) {
@@ -11,14 +12,18 @@ interface Generation {
         for (int i = 0; i < length; i++) {
             for (int j = 0; j < length; j++) {
                 numOfNeighbors = countNeighbors(grid, length, i, j);
+                // If cell is dead
                 if (grid[i][j] == ' ') {
+                    // If cell has 3 neighbors, resurrect the cell to life.
                     if (numOfNeighbors == 3) {
                         nextState[i][j] = 'O';
                     } else {
                         nextState[i][j] = grid[i][j];
                     }
                 } else {
-                    if (numOfNeighbors < 2 ||  numOfNeighbors > 3) {
+                    // If cell has less than 2 neighbor or greater than 3,
+                    // kill the cell.
+                    if (numOfNeighbors < 2 || numOfNeighbors > 3) {
                         nextState[i][j] = ' ';
                     } else {
                         nextState[i][j] = grid[i][j];
@@ -32,16 +37,17 @@ interface Generation {
     // Count Neighbors across the cell of a Matrix.
     private static int countNeighbors(char[][] grid, int size, int row, int col) {
         int countNeighbor = 0;
-        // isCol - keeps track either row/column is to be changed for traversing neighbor.
-        // isInc - keeps tracks when to increment or decrement the row/col to traverse neighbor.
-        boolean isCol = true,
-                isInc = true;
+        // isCol - keeps track either row/column is to be changed for traversing
+        // neighbor.
+        // isInc - keeps tracks when to increment or decrement the row/col to traverse
+        // neighbor.
+        boolean isCol = true, isInc = true;
         int[] index;
         for (int k = 0; k < 8; k++) {
             if (isInc) {
                 if (isCol) {
                     index = getIndexes(size, row, ++col);
-//                    System.out.println("Row: " + index[0] + " Col: " + index[1] + "First");
+                    // System.out.println("Row: " + index[0] + " Col: " + index[1] + "First");
                     if (grid[index[0]][index[1]] == 'O') {
                         countNeighbor++;
                     }
@@ -50,7 +56,7 @@ interface Generation {
                     }
                 } else {
                     index = getIndexes(size, ++row, col);
-//                    System.out.println("Row: " + index[0] + " Col: " + index[1] + "Second");
+                    // System.out.println("Row: " + index[0] + " Col: " + index[1] + "Second");
                     if (grid[index[0]][index[1]] == 'O') {
                         countNeighbor++;
                     }
@@ -60,14 +66,15 @@ interface Generation {
             } else {
                 if (isCol) {
                     index = getIndexes(size, row, --col);
-//                    System.out.println("Row: " + index[0] + " Col: " + index[1] + "Third");
+                    // System.out.println("Row: " + index[0] + " Col: " + index[1] + "Third");
                     if (grid[index[0]][index[1]] == 'O') {
                         countNeighbor++;
                     }
-                    if (k == 3) isCol = false;
+                    if (k == 3)
+                        isCol = false;
                 } else {
                     index = getIndexes(size, --row, col);
-//                    System.out.println("Row: " + index[0] + " Col: " + index[1] + "Fourth");
+                    // System.out.println("Row: " + index[0] + " Col: " + index[1] + "Fourth");
                     if (grid[index[0]][index[1]] == 'O') {
                         countNeighbor++;
                     }
@@ -93,8 +100,8 @@ interface Generation {
             i = i >= size ? size - i : i;
             j = j >= size ? size - j : j;
         }
-//        i = i < 0 ? size + i : i == size ? 0 : i;
-//        j = j < 0 ? size + j : j == size ? 0 : i;
+        // i = i < 0 ? size + i : i == size ? 0 : i;
+        // j = j < 0 ? size + j : j == size ? 0 : i;
         index[0] = i;
         index[1] = j;
         return index;
@@ -104,18 +111,26 @@ interface Generation {
 
 class GameOfLifeSetup implements Generation {
     private char[][] grid;
-    final private int length;
+    final private int length; // Number of rows and columns in grid
     final private Random rand;
-    final private int cellSize;
+    final private int cellSize; // Width and Height of each cell in GUI
 
     GameOfLifeSetup() {
-        this.length = 30;
+        this.length = 20;
+        if (this.length == 0) {
+            try {
+                throw new Exception("Length of Matrix is 0");
+            } catch (Exception e) {
+                System.out.println("Exception: " + e.getMessage());
+            }
+        }
         this.cellSize = 15;
         this.grid = new char[this.length][this.length];
         this.rand = new Random();
         setGrid();
     }
 
+    // Randomly initialising the grid for the 0th generation.
     private void setGrid() {
         for (int i = 0; i < this.length; i++) {
             for (int j = 0; j < this.length; j++) { 
@@ -137,6 +152,7 @@ class GameOfLifeSetup implements Generation {
         }
     }
 
+    // Updating grid for next generation
     public void generateGenerations(char[][] updatedGrid) {
         this.grid = updatedGrid;
         try {
@@ -149,6 +165,7 @@ class GameOfLifeSetup implements Generation {
         this.grid = Generation.generator(this.grid, this.length);
     }
 
+    // Count of number of alive cells in the matrix
     public int getAlive() {
         int count = 0;
         for (char[] row : this.grid) {
@@ -176,6 +193,7 @@ class GameOfLifeSetup implements Generation {
 
 public class Main {
     public static void main(String[] args) {
+        // Displaying JFrame by setting the class to true.
         new GameOfLife().setVisible(true);
     }
 }
